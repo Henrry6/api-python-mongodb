bc07a738458ded764718d1322e3d4eb2ad3e4f79
Henrry Alvarado <princedark393@gmail.com>
2023-01-23 18:32:40 -0500

    Mejoras en variables

--- servidor-pgsql/database/db.py
+++ servidor-pgsql/database/db.py
@@ -1,15 +1,19 @@
 import psycopg2
+from os import environ
+from dotenv import load_dotenv
 from psycopg2 import DatabaseError
+
+load_dotenv()
 
 
 def get_connection():
     try:
         return psycopg2.connect(
-            host='localhost',
-            port=5432,
-            user='postgres',
-            password='postgres',
-            dbname='inventary'
+            host=environ.get('DB_HOST'),
+            port=environ.get('DB_PORT'),
+            user=environ.get('DB_USER'),
+            password=environ.get('DB_PASSWORD'),
+            dbname=environ.get('DB_NAME')
         )
     except DatabaseError as ex:
         raise ex
